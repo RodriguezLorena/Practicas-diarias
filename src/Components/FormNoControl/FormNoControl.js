172faa213af5213672462e56f83defa31fc9679b
Lorena Rodriguez <loreleyro.89@gmail.com>
2022-09-22 19:36:28 -0300

    obtener valores del input

--- src/Components/FormNoControl/FormNoControl.js
+++ src/Components/FormNoControl/FormNoControl.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import { useState } from 'react'
+
 
 const FormNoControl = () => {
+    const [search, setSeatch] = useState("")
+
+    const submit= (e)=>{
+        e.preventDefault()
+        setSeatch(e.target.search.value)
+       }
   return (
-    <div>FormNoControl</div>
+    <div>
+        <h1>Formulario No controlado:</h1>
+        <form onSubmit={submit}>
+            <input type="text" name="search" autoComplete='off'/>
+            <button type='submit'>Imprimir</button>
+
+            <p>Se imprime: {search}</p>
+        </form>
+    </div>
   )
 }
 
