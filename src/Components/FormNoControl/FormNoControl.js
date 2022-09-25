7b6f417c2dc5d673298023c0a05d97e7b6cba771
Lorena Rodriguez <loreleyro.89@gmail.com>
2022-09-24 21:51:57 -0300

    iniciando estilos

--- src/Components/FormNoControl/FormNoControl.js
+++ src/Components/FormNoControl/FormNoControl.js
@@ -1,25 +1,32 @@
-import React from 'react'
-import { useState } from 'react'
-
+import React from "react";
+import { useState } from "react";
+import style from "./FormNoControl.module.css";
 
 const FormNoControl = () => {
-    const [search, setSeatch] = useState("")
+  const [search, setSeatch] = useState("");
 
-    const submit= (e)=>{
-        e.preventDefault()
-        setSeatch(e.target.search.value)
-       }
+  const submit = (e) => {
+    e.preventDefault();
+    setSeatch(e.target.search.value);
+  };
   return (
-    <div>
-        <h1>Formulario No controlado:</h1>
-        <form onSubmit={submit}>
-            <input type="text" name="search" autoComplete='off'/>
-            <button type='submit'>Imprimir</button>
-
-            <p>Se imprime: {search}</p>
-        </form>
+    <div className={style.content}>
+      <h1>Formulario No controlado:</h1>
+      <form onSubmit={submit}>
+        <textarea
+          type="text"
+          name="search"
+          autoComplete="off"
+          className={style.area}
+        />
+        <button type="submit" className={style.btn}>
+          Imprimir
+        </button>
+        <p className={style.span}>Se Imprime:</p>
+        <p className={style.impresion}> {search}</p>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default FormNoControl
+export default FormNoControl;
